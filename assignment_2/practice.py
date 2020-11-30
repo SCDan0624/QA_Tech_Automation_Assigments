11228cceea6cdefb10b778ca5b4ecdc2ebb04f85
SCDan0624 <dmayle1@hotmail.com>
2020-11-30 10:42:59 -0800

    Added url verification

--- assignment_2/practice.py
+++ assignment_2/practice.py
@@ -5,3 +5,10 @@ driver = webdriver.Chrome()
 
 # Navigate to “http://qatechhub.com
 driver.get('http://www.fb.com')
+
+# Check current URL
+url = driver.current_url
+print(url)
+
+assert url == 'https://www.facebook.com/'
+print("URL confirmed")
