0c03a0549af3c847e5dc6a85480360c8114ba269
SCDan0624 <dmayle1@hotmail.com>
2020-12-01 15:01:47 -0800

    Create new account button verified

--- assignment_2/practice.py
+++ assignment_2/practice.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 # Open the Firefox browser
 driver = webdriver.Chrome()
@@ -9,6 +12,15 @@ driver.get('http://www.fb.com')
 # Check current URL
 url = driver.current_url
 print(url)
-
 assert url == 'https://www.facebook.com/'
 print("URL confirmed")
+
+# Verify that there is a “Create an account” section on the page.
+create_account_css_locator = "a[id='u_0_2']"
+create_account_element = driver.find_element_by_css_selector(
+    create_account_css_locator)
+assert create_account_element.text == 'Create New Account'
+print("Create an account section confirmed")
+
+# Fill in the text boxes: First Name, Surname, Mobile Number or email address,
+#  “Re-enter mobile number”, new password.
