3c5ddb889b4dd22cf776a39e107d13b495f60dc6
SCDan0624 <dmayle1@hotmail.com>
2020-12-18 13:26:08 -0800

    added get input and click button to QATechPage class

--- assignment_1/pages/qa_tech_page.py
+++ assignment_1/pages/qa_tech_page.py
@@ -4,7 +4,17 @@ class QATechPage:
         self.driver = driver
 
     def type_into_input(self, text):
-        input = self.driver.find_element_by_id('search-field')
-        print(input)
-        input.send_keys(text)
+        inpt = self.driver.find_element_by_id('search-field')
+        print(inpt)
+        inpt.send_keys(text)
+        return None
+
+    def get_input_text(self):
+        inpt = self.driver.find_element_by_id('seach-field')
+        elem_text = inpt.get_attribute('value')
+        return elem_text
+
+    def search_button(self):
+        button = self.driver.find_element_by_id('search-button')
+        button.click()
         return None
