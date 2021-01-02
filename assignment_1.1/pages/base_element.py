5603495644f26d98fde792288410e93da0a92442
SCDan0624 <dmayle1@hotmail.com>
2021-01-01 16:05:46 -0800

    Added init to base_element

--- assignment_1.1/pages/base_element.py
+++ assignment_1.1/pages/base_element.py
@@ -1,1 +1,10 @@
 from selenium import webdriver
+
+
+class BaseElement(object):
+    def __init__(self, driver, locator, by):
+        self.driver = driver
+        self.locator = locator
+        self.by = by
+
+        self.web_element = None
